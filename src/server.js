a50822d71209b9cd11110d2c16bd4a147a129180
Fabiano Almeida <fabiano-horta@hotmail.com>
2023-03-17 13:15:36 -0300

    feat: Adicionando o server e o database

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const db = require("./database/db");
+const cors = require('cors');
+
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            
+    optionSuccessStatus:200,
+ }
+
+const routes = require("./routes/routes");
+
+app.use(cors(corsOptions))
+app.use(express.json());
+
+app.use("/", routes);
+
+db.sync().then(() => { console.log("A conexão com o banco de dados foi um sucesso.")}).catch((error) => console.log(error));
+
+const port = 3001 || 3333;
+app.listen(port, () => {
+    console.log("Rodando na porta: " + port);
+});
+
+module.exports = app;
